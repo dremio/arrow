37cfd5cab63f51e0d2baed797ef3350b6e319cf6
Ivan Chesnov <ivan.chesnov@dremio.com>
2023-06-30 21:03:29 +0300

    DX-67209 fixed tests

--- cpp/src/gandiva/tests/projector_test.cc
+++ cpp/src/gandiva/tests/projector_test.cc
@@ -2818,22 +2818,14 @@ TEST_F(TestProjector, TestAesEncryptDecrypt) {
 
   int num_records = 4;
 
+  const char* key_16_bytes = "12345678abcdefgh";
+  const char* key_24_bytes = "12345678abcdefgh12345678";
   const char* key_32_bytes = "12345678abcdefgh12345678abcdefgh";
-  const char* key_64_bytes =
-      "12345678abcdefgh12345678abcdefgh12345678abcdefgh12345678abcdefgh";
-  const char* key_128_bytes =
-      "12345678abcdefgh12345678abcdefgh12345678abcdefgh12345678abcdefgh12345678abcdefgh12"
-      "345678abcdefgh12345678abcdefgh12345678abcdefgh";
-  const char* key_256_bytes =
-      "12345678abcdefgh12345678abcdefgh12345678abcdefgh12345678abcdefgh12345678abcdefgh12"
-      "345678abcdefgh12345678abcdefgh12345678abcdefgh12345678abcdefgh12345678abcdefgh1234"
-      "5678abcdefgh12345678abcdefgh12345678abcdefgh12345678abcdefgh12345678abcdefgh123456"
-      "78abcdefgh";
 
   auto array_data = MakeArrowArrayUtf8({"abc", "some words", "to be encrypted", "hyah\n"},
                                        {true, true, true, true});
   auto array_key =
-      MakeArrowArrayUtf8({key_32_bytes, key_64_bytes, key_128_bytes, key_256_bytes},
+      MakeArrowArrayUtf8({key_16_bytes, key_24_bytes, key_32_bytes},
                          {true, true, true, true});
 
   auto array_holder_en = MakeArrowArrayUtf8({"", "", "", ""}, {true, true, true, true});
