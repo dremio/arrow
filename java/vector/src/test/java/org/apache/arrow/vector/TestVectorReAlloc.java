645f4cd4621ea06e5b72268249d3d09899288d16
Tim Hurski <tim.hurski@dremio.com>
2025-11-03 13:19:50 -0500

    DX-103340: Allow using 1GB+ data buffers in variable width vectors
    Backport of https://github.com/apache/arrow-java/pull/722

--- java/vector/src/test/java/org/apache/arrow/vector/TestVectorReAlloc.java
+++ java/vector/src/test/java/org/apache/arrow/vector/TestVectorReAlloc.java
@@ -23,6 +23,7 @@ import java.nio.charset.StandardCharsets;
 
 import org.apache.arrow.memory.BufferAllocator;
 import org.apache.arrow.memory.RootAllocator;
+import org.apache.arrow.memory.util.CommonUtil;
 import org.apache.arrow.vector.complex.DenseUnionVector;
 import org.apache.arrow.vector.complex.FixedSizeListVector;
 import org.apache.arrow.vector.complex.ListVector;
@@ -220,6 +221,17 @@ public class TestVectorReAlloc {
        */
       Assert.assertEquals(vector.getValueCapacity(), savedValueCapacity);
       Assert.assertEquals(vector.valueBuffer.capacity(), savedValueBufferSize);
+    }
+  }
+
+  @Test
+  public void testVariableReAllocAbove1GB() throws Exception {
+    try (final VarCharVector vector = new VarCharVector("", allocator)) {
+      long desiredSizeAboveLastPowerOf2 =
+          CommonUtil.nextPowerOfTwo(BaseVariableWidthVector.MAX_ALLOCATION_SIZE) / 2 + 1;
+      vector.reallocDataBuffer(desiredSizeAboveLastPowerOf2);
+
+      assertTrue(vector.getDataBuffer().capacity() >= desiredSizeAboveLastPowerOf2);
     }
   }
 
